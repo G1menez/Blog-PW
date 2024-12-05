82c1ab2dffa9675d9f332a2bc5f7771331dafe83
G1menez <161069377+G1menez@users.noreply.github.com>
2024-12-05 12:25:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+//pega o hamburguer
 var character = document.getElementById("character");
+//pega a boca
 var block = document.getElementById("block");
-var counter=0;
-function jump(){
-    if(character.classList == "animate"){return}
+//contador
+var counter = 0;
+//função pra pular
+function jump() {
+    //if da animação do hamburguer pulando
+    if (character.classList == "animate") { return }
     character.classList.add("animate");
-    setTimeout(function(){
+    setTimeout(function () {
         character.classList.remove("animate");
-    },300);
+    }, 300);
 }
-var checkDead = setInterval(function() {
+//var pra ver se perdeu o jogo
+var checkDead = setInterval(function () {
+    //hitbox do hamburguer
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if (blockLeft < 3 && blockLeft > -20 && characterTop >= 130) {
+        //mensagem de colisão e velocidade da boca
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
+        alert("Perdeu. Pontuação adquirida: " + Math.floor(counter / 100));
+        counter = 0;
+        block.style.animation = "block 0.8s infinite linear";
+    } else {
+        //contador de pontos
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
     }
 }, 10);
